7cb4987dd851f4f0fb8e617cb4000d7d40349add
hal3i <104970044+hal3i@users.noreply.github.com>
2026-07-05 20:28:46 +0900

    merge関数を実装

--- bpe_train.py
+++ bpe_train.py
@@ -5,3 +5,17 @@ def count_pairs(ids):
     for pair in zip(ids, ids[1:]):
         counts[pair] += 1
     return counts
+
+def merge(ids, pair, new_id):
+    merged_ids = []
+    i = 0
+
+    while i < len(ids):
+        if i < len(ids) - 1 and (ids[i], ids[i+1]) == pair:
+            merged_ids.append(new_id)
+            i += 2
+        else:
+            merged_ids.append(ids[i])
+            i += 1
+    
+    return merged_ids
--- bpe_train_test.py
+++ bpe_train_test.py
@@ -5,5 +5,8 @@ class BpeTrainTest(unittest.TestCase):
     def test_count_pairs(self):
         self.assertEqual(sut.count_pairs([1, 2, 3, 1, 2]), {(1, 2): 2, (2, 3): 1, (3, 1): 1});
 
+    def test_merge(self):
+        self.assertEqual(sut.merge([1, 2, 3, 1, 2], (1, 2), 4), [4, 3, 4])
+
 if __name__ == '__main__':
     unittest.main()
