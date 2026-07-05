393be0be3bbc6d20299ef9b2cb9bc918353b6569
hal3i <104970044+hal3i@users.noreply.github.com>
2026-07-05 22:06:24 +0900

    特殊トークンに対応したBPETokenizerクラスを実装

--- bpe_tokenizer.py
+++ bpe_tokenizer.py
@@ -1,24 +1,41 @@
 from bpe_train import merge
+import re
 
 class BPETokenizer:
-    def __init__(self, merge_rules):
+    def __init__(self, merge_rules, end_token="<|endoftext|>"):
         self.merge_rules = merge_rules
+        self.end_token = end_token
+        self.end_token_id = 256 + len(merge_rules)
 
         self.id_to_bytes = {i: bytes([i]) for i in range(256)}
 
         for (id1, id2), new_id in merge_rules.items():
             self.id_to_bytes[new_id] = self.id_to_bytes[id1] + self.id_to_bytes[id2]
 
+        self.id_to_bytes[self.end_token_id] = self.end_token.encode("utf-8")
+
         self.vocab_size = len(self.id_to_bytes)
 
-    def encode(self, text):
+    def _encode_text(self, text):
         ids = list(text.encode("utf-8"))
-
         for merge_pair, new_id in self.merge_rules.items():
             ids = merge(ids, merge_pair, new_id)
-
         return ids
-    
+
+    def encode(self, input_text):
+        pattern = '(' + re.escape(self.end_token) + ')'
+        texts = re.split(pattern, input_text)
+        all_ids = []
+
+        for text in texts:
+            if text == self.end_token:
+                all_ids.append(self.end_token_id)
+            else:
+                ids = self._encode_text(text)
+                all_ids.extend(ids)
+            
+        return all_ids
+
     def decode(self, ids):
         byte_list = [self.id_to_bytes[i] for i in ids]
 
--- bpe_tokenizer_test.py
+++ bpe_tokenizer_test.py
@@ -13,5 +13,14 @@ class TestBPETokenizer(unittest.TestCase):
         self.assertEqual(ids, [259, 108, 108, 111, 228, 184, 150, 231, 149, 140, 240, 159, 152, 129])
         self.assertEqual(decoded, "Hello世界😁")
 
+    def test_encode_decode_special_token(self):
+        sut = BPETokenizer({(105, 115): 256, (256, 32): 257, (105, 110): 258})
+
+        ids = sut.encode("Hello world!<|endoftext|>")
+        decoded = sut.decode(ids)
+
+        self.assertEqual(ids, [72, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100, 33, 259])
+        self.assertEqual(decoded, "Hello world!<|endoftext|>")
+
 if __name__ == '__main__':
     unittest.main()
