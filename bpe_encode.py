f8d282ba9bb7491360df7ae61482195d0b478578
hal3i <104970044+hal3i@users.noreply.github.com>
2026-07-06 15:36:18 +0900

    TinyCodesデータセット全体のエンコードとファイル保存のコードを追加

---
+++ bpe_encode.py
@@ -0,0 +1,17 @@
+import os, sys
+#os.chdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
+#sys.path.append('.')
+
+import numpy as np
+from bpe_tokenizer import BPETokenizer
+
+tokenizer = BPETokenizer.load_from("merge_rules.pkl")
+
+text = open("tiny_codes.txt").read()
+ids = tokenizer.encode(text, show_progress=True)
+
+ids_array = np.array(ids, dtype=np.uint16)
+ids_array.tofile("tiny_codes.bin")
+
+print(f"トークンID数: {len(ids_array)}")
+print(f"最初の20個のトークンID: {ids_array[:20]}")
